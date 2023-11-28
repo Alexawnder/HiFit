72c7bdfcc753b2612c32af667af345b0e4fccfcb
Alexander <alexanderlee389@gmail.com>
2023-11-28 15:05:00 -0800

    Update db_connection.mjs

--- db_connection.mjs
+++ db_connection.mjs
@@ -1,10 +1,14 @@
-import { createConnection } from 'mysql2';
+import { createConnection } from 'mysql';
 
 var conn= createConnection({
   host:"localhost", 
   user:"root", 
-  password:"password"
+  password:"password",
+  database: "hifit",
+  port:3306, 
 });
+
+
 
 
 conn.connect((err) => {
@@ -15,3 +19,22 @@ conn.connect((err) => {
   
     console.log('Connected to database!');
   });
+
+conn.query('INSERT INTO users (userID, username, password, email) VALUES (?, ?, ?, ?)', [2, 'Jane Snow', '67891', 'janesnow@example.com'], (err) => {
+  if (err) {
+    console.error('Error inserting user:', err.message);
+    return;
+  }
+
+  console.log('User inserted successfully!');
+});
+
+
+
+conn.query('SELECT * FROM users', (err, results) =>{
+  if (err) {
+    console.error('Error fetching users:', err.message);
+    return;
+  }
+  console.log('Retrieved users:', results);
+});
